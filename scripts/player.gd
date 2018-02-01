4e31b90080fbc2946d12aeb68a2bf6364e66b42c
bharathkotari@gmail.com <bharathkotari@gmail.com>
2018-02-01 12:31:11 +0530

    updating all

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ export var speed = 100
 export var jump=50
 var feet
 var player
+var flipped
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
@@ -11,7 +12,7 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	set_process(true)
-	player =get_node("Sprite 2")
+	player =get_node("player")
 	feet= get_node("RayCast2D")
 	feet.add_exception(self)
 	#player.set_blend_mode(1)
@@ -19,10 +20,24 @@ func _ready():
 
 func _process(delta):
 	if feet.is_colliding():
+		
 		if Input.is_action_pressed("ui_up"):
 			set_axis_velocity(Vector2(0,-jump))
-		if Input.is_action_pressed("ui_right"):
+			player.set_animation("attack")
+		
+		elif Input.is_action_pressed("ui_right"):
 			set_axis_velocity(Vector2(speed,0))
-		if Input.is_action_pressed("ui_left"):
+			player.set_animation("walk_left")
+			flipped=1
+			player.set_flip_h(true)
+			
+		elif Input.is_action_pressed("ui_left"):
 			set_axis_velocity(Vector2(-speed,0))
+			if flipped==1 :
+				player.set_flip_h(false)
+			player.set_animation("walk_left")
+		else:
+			player.set_animation("idle")
+		
+
 	

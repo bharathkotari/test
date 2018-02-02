923a3b1693288be1df624d760c43f121715b4f36
bharathkotari@gmail.com <bharathkotari@gmail.com>
2018-02-02 15:53:26 +0530

    updating

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,12 @@
 extends RigidBody2D
 export var speed = 100
+export var acc =2
 export var jump=50
 var feet
 var player
 var flipped
 var cam
+var time =0
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
@@ -17,11 +19,12 @@ func _ready():
 	feet= get_node("RayCast2D")
 	feet.add_exception(self)
 	cam=get_node("Camera2D")
-	#cam.set_zoom(
+	cam.set_zoom((Vector2(400/get_node("player").get_viewport_rect().size.x,400/get_node("player").get_viewport_rect().size.x)))
 	#player.set_blend_mode(1)
 
 
 func _process(delta):
+	
 	if feet.is_colliding():
 		
 		if Input.is_action_pressed("ui_up"):
@@ -29,17 +32,21 @@ func _process(delta):
 			player.set_animation("attack")
 		
 		elif Input.is_action_pressed("ui_right"):
-			set_axis_velocity(Vector2(speed,0))
+			time+=1
+			set_axis_velocity(Vector2(speed+acc*time,0))
+			
 			player.set_animation("walk_left")
 			flipped=1
 			player.set_flip_h(true)
 			
 		elif Input.is_action_pressed("ui_left"):
-			set_axis_velocity(Vector2(-speed,0))
+			time+=1
+			set_axis_velocity(Vector2(-speed-acc*time,0))
 			if flipped==1 :
 				player.set_flip_h(false)
 			player.set_animation("walk_left")
 		else:
+			time =0
 			player.set_animation("idle")
 		
 
